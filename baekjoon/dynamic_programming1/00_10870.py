1fa5a4c37195c90c5292de99f7fa3fa2cf7c99b8
MJ <ydaispaxero@naver.com>
2022-03-28 22:03:52 +0900

    baekjoon: 00_10870 피보나치 수 5

---
+++ baekjoon/dynamic_programming1/00_10870.py
@@ -0,0 +1,22 @@
+"""
+피보나치 수 5
+
+https://www.acmicpc.net/problem/10870
+"""
+
+import sys
+
+sys.stdin = open("input.txt", "rt")
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+    if n == 0:
+        print(0)
+        exit(0)
+
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 1]
+    print(dp[n])
